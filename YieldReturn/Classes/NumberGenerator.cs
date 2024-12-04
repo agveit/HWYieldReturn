b7a4b0a1a8b2606c88b3b3a27af7f206be0e55a4
agveit <agveit.a@mail.ru>
2024-12-04 17:32:35 +0500

    Добавьте файлы проекта.

---
+++ YieldReturn/Classes/NumberGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace YieldReturn.Classes
+{
+    class NumberGenerator
+    {
+        public IEnumerable<int> GenerateNumbers(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
---
+++ YieldReturn/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using YieldReturn.Classes;
+
+namespace YieldReturn
+{
+    public partial class MainWindow : Window
+    {
+        private NumberGenerator _generator;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _generator = new NumberGenerator();
+        }
+
+        private async void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            NumberListBox.Items.Clear();
+            int.TryParse(StartTextBox.Text, out int start);
+            int.TryParse(EndTextBox.Text, out int end);
+            var numbers = _generator.GenerateNumbers(start, end);
+            foreach (int number in numbers)
+            {
+                NumberListBox.Items.Add(number);
+                await Task.Delay(500);
+            }
+            MessageBox.Show("Генерация завершена.");
+
+        }
+
+        private void StartTextBoxGotFocus(object sender, RoutedEventArgs e)
+        {
+            StartTextBox.Clear();
+        }
+
+        private void EndTextBoxGotFocus(object sender, RoutedEventArgs e)
+        {
+            EndTextBox.Clear();
+        }
+    }
+}
